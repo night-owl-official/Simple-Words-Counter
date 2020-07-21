c953ad96621ffc5b5b6f5d28f2ed3ea0f7e680eb
NightOwl <daniele.grieco@live.com>
2020-07-21 20:34:15 +0200

    Implemented function that reads a file into a string

--- WordCounter/Helpers.cpp
+++ WordCounter/Helpers.cpp
@@ -3,7 +3,21 @@
 #include <fstream>
 
 std::string ReadFile(const std::string& filePath) {
-	return nullptr;
+	// Try to open the given file
+	std::ifstream inputFile;
+	inputFile.open(filePath);
+
+	// Don't continue if the file didn't open correctly
+	if (!inputFile.is_open())	return "";
+
+	// Keep reading until the end of the file
+	std::string textFile = "";
+	while (!inputFile.eof())	std::getline(inputFile, textFile, '\0');
+
+	// Close the file
+	inputFile.close();
+
+	return textFile;
 }
 
 unsigned int GetWordsCount(const std::string& txt) {
--- WordCounter/Main.cpp
+++ WordCounter/Main.cpp
@@ -1,7 +1,20 @@
 #include "Helpers.h"
 
 int main(int argc, char** argv) {
-	
+	// Store the file to analyze
+	TextFile randomFile;
+	randomFile.text = ReadFile(argv[1]);
+
+	// Make sure the given file path is correct
+	if (randomFile.text != "") {
+		// Count the words in the file
+		randomFile.wordsCount = GetWordsCount(randomFile.text);
+
+		printf("There are %d words in this file", randomFile.wordsCount);
+	} else {
+		// File path was probably wrong
+		printf("There was an error in reading the file! Make sure the file path is correct.");
+	}
 
 	return 0;
 }
